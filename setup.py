09216ad7e01bcbc9449bf5081a375f6f880bd655
syakoo <nichijou4009@gmail.com>
2020-12-15 15:05:30 +0900

    fix: package name.

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ def _requires_from_file(filename):
 
 setuptools.setup(
     name="azfunc_extensions",
-    version="0.2",
+    version="0.3",
     author="syakoo",
     author_email="sakodata0318@gmail.com",
     description="Azure functions' extension tools for Python.",
     url="https://github.com/syakoo/azfunc-extensions",
-    packages=["azfunc-extensions"],
-    package_dir={"azfunc-extensions": "src"},
+    packages=["azfunc_extensions"],
+    package_dir={"azfunc_extensions": "src"},
     install_requires=_requires_from_file('requirements.txt'),
     python_requires=">=3.8"
 )
